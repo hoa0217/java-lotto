c21d4e353594c25d1bb1eae4186e439034196384
hoa0217 <gjwjdghk123@naver.com>
2023-05-17 14:00:48 +0900

    refactor: 추상메서드 BiPredicate로 리팩토링

--- src/main/java/study/lottogame/domain/Rank.java
+++ src/main/java/study/lottogame/domain/Rank.java
@@ -1,51 +1,24 @@
 package study.lottogame.domain;
 
 import java.util.Arrays;
+import java.util.function.BiPredicate;
 
 public enum Rank {
-  MISS(0, 0) {
-    @Override
-    public boolean matches(int countOfMatch, boolean matchBonus) {
-      return countOfMatch == 0;
-    }
-  },
-  FIFTH(3, 5_000) {
-    @Override
-    public boolean matches(int countOfMatch, boolean matchBonus) {
-      return countOfMatch == 3;
-    }
-  },
-  FOURTH(4, 50_000) {
-    @Override
-    public boolean matches(int countOfMatch, boolean matchBonus) {
-      return countOfMatch == 4;
-    }
-  },
-  THIRD(5, 1_500_000) {
-    @Override
-    public boolean matches(int countOfMatch, boolean matchBonus) {
-      return countOfMatch == 5 && !matchBonus;
-    }
-  },
-  SECOND(5, 30_000_000) {
-    @Override
-    public boolean matches(int countOfMatch, boolean matchBonus) {
-      return countOfMatch == 5 && matchBonus;
-    }
-  },
-  FIRST(6, 2_000_000_000) {
-    @Override
-    public boolean matches(int countOfMatch, boolean matchBonus) {
-      return countOfMatch == 6;
-    }
-  };
+  MISS(0, 0, (countOfMatch, matchBonus) -> countOfMatch == 0),
+  FIFTH(3, 5_000, (countOfMatch, matchBonus) -> countOfMatch == 3),
+  FOURTH(4, 50_000, (countOfMatch, matchBonus) -> countOfMatch == 4),
+  THIRD(5, 1_500_000, (countOfMatch, matchBonus) -> countOfMatch == 5 && !matchBonus),
+  SECOND(5, 30_000_000, (countOfMatch, matchBonus) -> countOfMatch == 5 && matchBonus),
+  FIRST(6, 2_000_000_000, (countOfMatch, matchBonus) -> countOfMatch == 6);
 
   private int countOfMatch;
   private int winningMoney;
+  private BiPredicate<Integer, Boolean> matchPredicate;
 
-  private Rank(int countOfMatch, int winningMoney) {
+  private Rank(int countOfMatch, int winningMoney, BiPredicate<Integer, Boolean> matchPredicate) {
     this.countOfMatch = countOfMatch;
     this.winningMoney = winningMoney;
+    this.matchPredicate = matchPredicate;
   }
 
   public int getCountOfMatch() {
@@ -56,12 +29,14 @@ public enum Rank {
     return winningMoney;
   }
 
-  public abstract boolean matches(int countOfMatch, boolean matchBonus);
+  public boolean matches(int countOfMatch, boolean matchBonus) {
+    return matchPredicate.test(countOfMatch, matchBonus);
+  }
 
   public static Rank valueOf(int countOfMatch, boolean matchBonus) {
     return Arrays.stream(Rank.values())
-        .filter(rank -> rank.matches(countOfMatch, matchBonus))
-        .findFirst()
-        .orElse(Rank.MISS);
+            .filter(rank -> rank.matches(countOfMatch, matchBonus))
+            .findFirst()
+            .orElse(Rank.MISS);
   }
 }
